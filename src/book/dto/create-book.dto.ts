5162de3e563e6ab81400f8e8a139ea8909e21b22
Renan <renancvr777@gmail.com>
2025-06-08 20:20:34 -0300

    feat: request body validation with DTOs

--- src/book/dto/create-book.dto.ts
+++ src/book/dto/create-book.dto.ts
@@ -1,1 +1,21 @@
-export class CreateBookDto {}
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { Exclude } from 'class-transformer';
+import { Book } from '../entities/book.entity';
+import { IsEnum, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateBookDto implements Book {
+  @Exclude()
+  id: number;
+
+  @IsString()
+  @IsNotEmpty()
+  titulo: string;
+
+  @IsString()
+  @IsNotEmpty()
+  author: string;
+
+  @IsString()
+  @IsEnum(['disponivel', 'indisponivel'])
+  status: string;
+}
