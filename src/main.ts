d0cc11c54489bcb6d5f6359a62f86586e0a331c3
renanjava <renancvr777@gmail.com>
2025-06-09 21:29:56 -0300

    feat: add kafka connection with book microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
 
+const logger = new Logger('Main');
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          clientId: 'book',
+          brokers: ['localhost:9092'],
+        },
+        consumer: {
+          groupId: 'book-consumer',
+          allowAutoTopicCreation: true,
+        },
+      },
+    },
+  );
+  logger.log('Book microservice is starting...');
+  await app.listen();
 }
-bootstrap();
+void bootstrap();
